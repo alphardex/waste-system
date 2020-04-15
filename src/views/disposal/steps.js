02a7f5468d364bd6492247db457bd680a0867dae
alphardex <2582347430@qq.com>
2020-04-15 20:26:24 +0800

    add tutorial

---
+++ src/views/disposal/steps.js
@@ -0,0 +1,69 @@
+const steps = [
+  {
+    element: '#container',
+    popover: {
+      title: '地图',
+      description: '用于展示坐标点，单击点可编辑点信息',
+      position: 'bottom'
+    }
+  },
+  {
+    element: '.dot-reader',
+    popover: {
+      title: '导入点坐标',
+      description: '导入事先用ArcGIS导出的点坐标集合',
+      position: 'bottom'
+    }
+  },
+  {
+    element: '.clear',
+    popover: {
+      title: '清空点坐标',
+      description: '清空地图上所有的点',
+      position: 'bottom'
+    }
+  },
+  {
+    element: '.weight-range-control',
+    popover: {
+      title: '重量范围(吨)',
+      description: '以重量范围(吨)来筛选投放点',
+      position: 'bottom'
+    }
+  },
+  {
+    element: '.add-vehicle',
+    popover: {
+      title: '添加垃圾车',
+      description: '开启则能在地图上点击时添加垃圾车点',
+      position: 'bottom'
+    }
+  },
+  {
+    element: '.select-point-mode',
+    popover: {
+      title: '点选择',
+      description: '开启则能在地图上选中想要的点',
+      position: 'bottom'
+    }
+  },
+  {
+    element: '.clear-select',
+    popover: {
+      title: '清空点选择',
+      description: '取消选中的所有点',
+      position: 'bottom'
+    }
+  },
+  {
+    element: '.calc-distance',
+    popover: {
+      title: '计算两点间距离',
+      description: '计算选中的两点的距离',
+      position: 'bottom'
+    }
+  }
+]
+
+export default steps
+
